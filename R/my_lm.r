b5cda0a5170f54caf1143e4556c6d5f334421a2a
emilkere <emilsvetkere@gmail.com>
2020-12-02 19:56:59 -0800

    Initial Package Setup

---
+++ R/my_lm.r
@@ -0,0 +1,54 @@
+#' Linear Model Fit
+#'
+#' This function fits linear model to data passed as a data frame.
+#
+#' @param formula An object of class \code{formula} which is a symbolic description
+#'    of the model to be fitted.
+#' @param df A data frame containing the variables used in the model.
+#' @keywords lm
+#'
+#' @return A table with rows for each coefficient (including the Intercept)
+#'   and columns for the Estimate, Std. Error, t value, and Pr(>|t|).
+#'
+#' @examples
+#' data(mtcars)
+#' my_lm(mpg ~ hp + wt, mtcars)
+#'
+#' ## no intercept
+#' my_lm(mpg ~ hp + wt - 1, mtcars)
+#'
+#' ## interaction
+#' my_lm(mpg ~ hp * wt, mtcars)
+#'
+#' @export
+my_lm <- function(formula, df) {
+  X <- model.matrix(formula, df)
+  Y <- model.response(model.frame(formula, df))
+
+  X_t <- t(X)
+  kern_X <- solve(X_t %*% X)
+  beta_hat <- kern_X %*% X_t %*% Y
+  n_cov <- nrow(beta_hat)
+
+  dgf <- length(Y) - n_cov
+
+  sigma_2 <- sum((Y - X %*% beta_hat)^2 / dgf)
+  std_err <- sqrt(diag(sigma_2 * kern_X))
+  t_val <- beta_hat / std_err
+
+  pr_t <- rep(0, n_cov)
+  for (j in 1:n_cov) {
+    pr_t[j] <- 2 * pt(abs(t_val[j]), dgf, lower.tail = F)
+  }
+
+  # setup return values
+  mret <- cbind(beta_hat)
+  mret <- cbind(mret, std_err)
+  mret <- cbind(mret, t_val)
+  mret <- cbind(mret, pr_t)
+
+  # set column names
+  colnames(mret) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+
+  return(mret)
+}
---
+++ R/my_t.test.r
@@ -0,0 +1,54 @@
+#' Student's t-test
+#'
+#' This function performs a one sample Student's t-test on vector data.
+#'
+#' @param x Numeric vector of data to be tested.
+#' @param alternative Character string specifying the alternative hypothesis.
+#'      Accepts \code{"two.sided"}, \code{"less"}, or \code{"greater"},
+#'      defaults to \code{"two.sided"}.
+#' @param mu Numeric input indicating the null hypothesis value of the mean,
+#'      defaults to 0.
+#' @keywords t.test
+#'
+#' @return A list with the following elements: \code{test_stat} representing
+#'   the numeric test statistic, \code{df} representing the degrees
+#'   of freedom, \code{alternative} with the value of the parameter \code{alternative},
+#'   and \code{p_val} the numeric p-value.
+#'
+#' @examples
+#' tdata <- rnorm(100, 5, 1)
+#' my_t.test(tdata, mu = 5)
+#'
+#' tdata <- rnorm(1000, 1, 1)
+#' my_t.test(tdata, mu = 1, alternative = "greater")
+#'
+#' @export
+my_t.test <- function(x, alternative = "two.sided", mu = 0) {
+  # validate input
+  alternative <- str_to_lower(alternative)
+  alt_opptions <- c("two.sided", "less", "greater")
+  if (!alternative %in% alt_opptions) {
+    stop(paste(
+      "Argument: alternative, Value:", alternative,
+      ' is not supported. Supported values are "two.sided", "less", or "greater".'
+    ))
+  }
+
+  sample_size <- length(x)
+  theta_0 <- mu
+  theta_hat <- mean(x)
+  se_theta_hat <- sd(x) / sqrt(sample_size)
+  # calculate the t statistic
+  t_stat <- (theta_hat - theta_0) / se_theta_hat
+  df <- sample_size - 1
+
+  p_val <- switch(alternative,
+                  two.sided = 2 * pt(abs(t_stat), df, lower.tail = F),
+                  less = pt(t_stat, df, lower.tail = T),
+                  greater = pt(t_stat, df, lower.tail = F)
+  )
+
+  # set up the return list
+  lret <- list("test_stat" = t_stat, "df" = df, "alternative" = alternative, "p-val" = p_val)
+  return(lret)
+}
