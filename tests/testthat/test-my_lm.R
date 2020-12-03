41d08485db4e5962067a76e358838b59c1074810
emilkere <emilsvetkere@gmail.com>
2020-12-03 08:34:03 -0800

    styling tests

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -3,13 +3,13 @@ test_that("numeric output throws error", {
 })
 
 test_that("numeric output throws error", {
-  expect_error(my_lm(x~y, df=1))
+  expect_error(my_lm(x ~ y, df = 1))
 })
 
 test_that("lm works", {
-  x = c(1,2,1,3,1,4)
-  y = c(1,1,2,2,3,3)
-  df = data.frame(x=x,y=y)
-  mlm = my_lm(y~x, df)
-  expect_equal(mlm[1,1], 1.5)
+  x <- c(1, 2, 1, 3, 1, 4)
+  y <- c(1, 1, 2, 2, 3, 3)
+  df <- data.frame(x = x, y = y)
+  mlm <- my_lm(y ~ x, df)
+  expect_equal(mlm[1, 1], 1.5)
 })
